d529a240554275d3ebfd8db3cb981e91aa580595
ming2 <ming2@Mingders-Mac-mini.local>
2025-06-22 11:31:05 +0800

    fork from z

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,12 +2,17 @@ import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
 import "@nomicfoundation/hardhat-chai-matchers";
 import "@tenderly/hardhat-tenderly";
+import "@nomicfoundation/hardhat-verify";
+
 import * as dotenv from "dotenv";
 dotenv.config();
  
 const config: HardhatUserConfig = {
+  defaultNetwork: "sepolia", 
   networks: {
-      sepolia: {
+    hardhat: {
+    },
+    sepolia: {
       url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
       accounts: [process.env.PRIVATE_KEY],
     },
@@ -25,7 +30,21 @@ const config: HardhatUserConfig = {
   tenderly: {
     project: "project/z",
     username: "mingderwang2",
-    privateVerification: false, // Public verification
+    privateVerification: false,
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: process.env.ETHERSCAN_API_KEY,
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 40000
   },
 };
 
